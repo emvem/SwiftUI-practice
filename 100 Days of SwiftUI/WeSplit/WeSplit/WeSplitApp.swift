dca92ac5db2e9d06a6c9b19eb1b72a65109c825d
Em Vadim <em.vadim@upservice.io>
2021-03-03 14:09:13 +0600

    day 16 introduction

---
+++ 100 Days of SwiftUI/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vadim Em on 03.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
