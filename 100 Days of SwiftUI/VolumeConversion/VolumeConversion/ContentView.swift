7f3aa9e40caa416b4feb24d9ab3979b15fad5545
Em Vadim <em.vadim@upservice.io>
2021-03-06 00:40:26 +0600

    day 19 volumes amount set

--- 100 Days of SwiftUI/VolumeConversion/VolumeConversion/ContentView.swift
+++ 100 Days of SwiftUI/VolumeConversion/VolumeConversion/ContentView.swift
@@ -8,25 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    enum Volume: String, CaseIterable, Identifiable {
+        case milliliters
+        case cups
+        case pints
+        case litters
+        case gallons
+        
+        var id: String { self.rawValue }
+        
+        var amountInMilliliters: Double {
+            return getAmountInMilliliters(for: self)
+        }
+        
+        var milliliters: String {
+            return "\(Int(amountInMilliliters)) ml"
+        }
+        
+        private func getAmountInMilliliters(for volume: Volume) -> Double {
+            switch volume {
+            case .milliliters:
+                return 1
+            case .cups:
+                return getAmountInMilliliters(for: .milliliters) * 250
+            case .pints:
+                return getAmountInMilliliters(for: .cups) * 2
+            case .litters:
+                return getAmountInMilliliters(for: .pints) * 2
+            case .gallons:
+                return getAmountInMilliliters(for: .litters) * 4
+            }
+        }
+        
+    }
     
-    @State private var input = ""
-    @State private var output = ""
+    @State private var input: Volume = .milliliters
+    @State private var output: Volume = .cups
+    
+    private let volumes: [Volume] = [.milliliters, .litters, .cups, .pints, .gallons]
     
     var body: some View {
         NavigationView {
             Form {
-                Section {
-                    Picker("Input", selection: $input, content: {
-                        Group {
-                            Text("0")
-                            Text("1")
+                Section(header: Text("Input/Output selection")) {
+                    Picker(input.rawValue, selection: $input, content: {
+                        ForEach(volumes) {
+                            Text($0.rawValue)
                         }
                     })
-                    Picker("Output", selection: $input, content: {
-                        Text(input)
+                    Picker(output.rawValue, selection: $output, content: {
+                        ForEach(volumes) {
+                            Text($0.rawValue)
+                        }
                     })
                 }
-                Text("Hello, world!")
+
+                Section(header: Text("Input/Output descrription")) {
+                    HStack {
+                        Text("\(input.rawValue)")
+                        Text("\(input.milliliters)")
+                    }
+                    HStack {
+                        Text("\(output.rawValue)")
+                        Text("\(output.milliliters)")
+                    }
+                }
             }
             .navigationBarTitle("Volume Conversion")
         }
