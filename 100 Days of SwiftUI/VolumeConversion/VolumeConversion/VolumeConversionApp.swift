079654c7ad43cb93063661df487d6431b26cf116
Em Vadim <em.vadim@upservice.io>
2021-03-06 00:03:01 +0600

    day 19 Volume Conversion

---
+++ 100 Days of SwiftUI/VolumeConversion/VolumeConversion/VolumeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConversionApp.swift
+//  VolumeConversion
+//
+//  Created by Vadim Em on 06.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
