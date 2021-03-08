379e346642d9d7b57ed94863906e5ac5c2defe48
Em Vadim <em.vadim@upservice.io>
2021-03-09 03:09:17 +0600

    News App news links

--- Tundsdev/NewsApp/NewsApp/Views/HomeView.swift
+++ Tundsdev/NewsApp/NewsApp/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @Environment(\.openURL) var openURL
     @StateObject var viewModel = NewsViewModelImpl(service: NewsServiceImpl())
     
     var body: some View {
@@ -20,8 +21,11 @@ struct HomeView: View {
                 ErrorView(error: error, handler: viewModel.getArticles)
             case .success(let articles):
                 NavigationView {
-                    List(articles) {
-                        ArticleView(article: $0)
+                    List(articles) { item in
+                        ArticleView(article: item)
+                            .onTapGesture {
+                                load(url: item.url)
+                            }
                     }
                     .navigationTitle(Text("News"))
                 }
@@ -30,6 +34,14 @@ struct HomeView: View {
             viewModel.getArticles()
         }
     }
+    
+    func load(url: String?) {
+        guard let link = url,
+              let url = URL(string: link) else { return }
+        
+        openURL(url)
+    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
