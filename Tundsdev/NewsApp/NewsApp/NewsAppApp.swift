2df598aa658a5c87927874ca31aa514ae435924b
Em Vadim <em.vadim@upservice.io>
2021-03-09 01:47:02 +0600

    Tundsdev News App

---
+++ Tundsdev/NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Vadim Em on 09.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
