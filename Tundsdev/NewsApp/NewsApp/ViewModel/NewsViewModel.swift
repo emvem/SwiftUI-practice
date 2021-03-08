f2d1996052a4c22d0195b9ab906c120ae633bd3a
Em Vadim <em.vadim@upservice.io>
2021-03-09 02:22:04 +0600

    News App News View Model

--- Tundsdev/NewsApp/NewsApp/ViewModel/NewsViewModel.swift
+++ Tundsdev/NewsApp/NewsApp/ViewModel/NewsViewModel.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import Combine
 
-class NewsViewModel {
+protocol NewsViewModel {
+    func getArticles()
+}
+
+class NewsViewModelImpl: ObservableObject, NewsViewModel {
     
+    private let service: NewsService
+    
+    private(set) var articles = [Article]()
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published private(set) var state: ResultState = .loading
+    
+    init(service: NewsService) {
+        self.service = service
+    }
+    
+    func getArticles() {
+        self.state = .loading
+        let cancellable = service
+            .request(from: NewsAPI.getNews)
+            .sink { (res) in
+                switch res {
+                case .finished:
+                    self.state = .success(content: self.articles)
+                case .failure(let error):
+                    self.state = .failed(error: error)
+                }
+            } receiveValue: { (response) in
+                self.articles = response.articles
+            }
+
+        
+    }
 }
