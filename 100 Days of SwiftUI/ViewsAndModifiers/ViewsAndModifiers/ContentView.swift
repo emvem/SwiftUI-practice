a89e6ec15b2e239db283925c677c4708b64cf4c7
Em Vadim <mr.vadim134@gmail.com>
2021-03-09 12:44:20 +0600

    day 24 challenge 1 custom view modifier

--- 100 Days of SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 100 Days of SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,38 +7,24 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows) { row in
-                HStack {
-                    ForEach(0..<columns) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(Color.blue)
     }
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        GridStack(rows: 4, columns: 4) { (row, col) in
-            HStack {
-                Image(systemName: "\(row * 4 + col).circle")
-                Text("R\(row) C\(col)")
-            }
-        }
+        Text("Hello world")
+            .titleStyle()
     }
 }
 
