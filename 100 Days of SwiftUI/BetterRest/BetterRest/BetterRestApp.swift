cd06af91249f2dc36c61739e5fb5314f4ad46787
Em Vadim <mr.vadim134@gmail.com>
2021-03-14 13:56:20 +0600

    day 26 Better Rest

---
+++ 100 Days of SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vadim Em on 14.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
