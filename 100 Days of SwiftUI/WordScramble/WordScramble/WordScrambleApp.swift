48cc4cd6b41391a9091793a989b189b01ed78cdc
Em Vadim <mr.vadim134@gmail.com>
2021-03-15 23:57:26 +0600

    day 29 Word Scramble

---
+++ 100 Days of SwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Vadim Em on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
