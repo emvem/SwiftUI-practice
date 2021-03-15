2ab254f1c6637c2d21085f749afbe016a95a65c1
Em Vadim <mr.vadim134@gmail.com>
2021-03-16 00:13:25 +0600

    day 29 working with string

--- 100 Days of SwiftUI/WordScramble/WordScramble/ContentView.swift
+++ 100 Days of SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -13,14 +13,29 @@ struct ContentView: View {
     
     var body: some View {
         
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
+        let input = """
+            a
+            b
+            c
+        """
+        
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
         if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
             if let fileContents = try? String(contentsOf: fileURL) {
                 
             }
         }
         
-        return List(people, id: \.self) {
-            Text("Hello, world! \($0)")
+        return List(letters, id: \.self) {
+            Text("Hello, world! \($0.trimmingCharacters(in: .whitespacesAndNewlines))")
         }
         .listStyle(GroupedListStyle())
     }
