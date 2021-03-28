99fd0f324c83de8d19ec2e044ac3c05129d1acbf
Em Vadim <mr.vadim134@gmail.com>
2021-03-28 14:22:40 +0600

    day 32 Animations

---
+++ 100 Days of SwiftUI/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Vadim Em on 28.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
